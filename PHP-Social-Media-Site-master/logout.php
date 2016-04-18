b3678c5a8d40ad683de822a51a5b47a017971c52
blgold <blgoldadam@gmail.com>
2016-04-18 11:48:56 -0500

    Added files via upload
    Change color scheme. Redirected page on login and log out

---
+++ PHP-Social-Media-Site-master/logout.php
@@ -0,0 +1,17 @@
+<?php // Example 26-12: logout.php
+  require_once 'header.php';
+
+  if (isset($_SESSION['user']))
+  {
+    destroySession();
+    echo "<div class='main'>You have been logged out. Please " .
+         "<a href='index.php'>click here</a> to refresh the screen.";
+		  echo '<meta HTTP-EQUIV="REFRESH" content="0; url=login.php?">';
+  }
+  else echo "<div class='main'><br>" .
+            "You cannot log out because you are not logged in";
+?>
+
+    <br><br></div>
+  </body>
+</html>
